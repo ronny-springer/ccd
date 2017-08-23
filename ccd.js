bffe635fd1e2518831c25e0d8bfefaa0d402e6aa
Ronny Springer <ronny.springer@signavio.com>
2017-08-23 13:33:05 +0200

    clean up whitespaces and line indents

--- ccd.js
+++ ccd.js
@@ -1,20 +1,20 @@
 var program = require('commander')
-	fr = require('./script/FileReader');
+    fr = require('./script/FileReader');
 
 program
-  	.version('1.0.0');
+    .version('1.0.0');
 
 program
-	.option('-f, --file', 'read css file')
-	.action(function(cmd, options) {
-    	fr.readFile('%s', function (error, data) {
-    		if (!error) {
-    			console.log(data);
-    		} else {
-    			console.log(error);
-    		}
-    	});
-  	});
+    .option('-f, --file', 'read css file')
+    .action(function(cmd, options) {
+        fr.readFile('%s', function (error, data) {
+            if (!error) {
+                console.log(data);
+            } else {
+                console.log(error);
+            }
+        });
+    });
 
 program
-	.parse(process.argv);
+   .parse(process.argv);
--- script/FileReader.js
+++ script/FileReader.js
@@ -4,11 +4,11 @@ var fs = require('fs'),
 function FileReader() {}
 
 FileReader.prototype.readFile = function ( file, callback ) {
-	var filePath = path.join(__dirname, file || ''),
-		fileEncoding = 'utf-8',
-		noop = function () {};
+    var filePath = path.join(__dirname, file || ''),
+        fileEncoding = 'utf-8',
+        noop = function () {};
 
-	fs.readFile(filePath, {encoding: fileEncoding}, callback || noop);
+    fs.readFile(filePath, {encoding: fileEncoding}, callback || noop);
 };
 
 exports = module.exports = new FileReader();
