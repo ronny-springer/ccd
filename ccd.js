ae2bd15939e499001da2ede353a2ced3dc679cf4
Ronny Springer <springer.ronny@gmail.com>
2017-08-27 14:17:48 +0200

    parse comments from CSS files

--- ccd.js
+++ ccd.js
@@ -1,5 +1,6 @@
 const program = require('commander')
 const fr = require('./script/FileReader')
+const parser = require('./script/Parser')
 
 program
     .version('1.0.0')
@@ -12,10 +13,12 @@ if (program.file) {
     console.log('file', program.file)
 
     fr.readFile(program.file)
+        .then(parser.grabComment)
         .then(( data ) => {
-            console.log('ok', data)
+        	console.log('return', data)
         })
         .catch(( error ) => {
             console.log('no', error)
         })
+
 }
