10ee0447d8a3a5404e9a2cd6014ac4d9e11e02cb
Ronny Springer <springer.ronny@gmail.com>
2017-08-30 03:18:18 +0200

    convert the parsed comment lines into wanted semantics

--- script/Parser.js
+++ script/Parser.js
@@ -2,14 +2,45 @@ const esprima = require('esprima')
 
 function Parser () {}
 
-const normalizeCommentLines = ( data ) => {
-    return data
+// convert block comment strings into a list of each comment line
+const normalizeBlockComments = ( data ) => {
+    let comment = {}
+
+     data
+
         // create a list of comment lines based on line breaks
         .split(/\n/)
+
         // remove whitespaces and leading Asterisks 
-        .map(item => item.replace(/[\s,\/\**]/g, '').trim()) 
+        .map(item => item.replace(/[\s,\/\**]/g, '').trim())
+
         // remove empty items at the list
         .filter(item => item.length)
+
+        // convert the parsed comment lines into wanted semantics
+        .map(item => {
+            const markerChar = item.charAt(0)
+            const commentValue = item.substr(1, item.length)
+
+            switch (markerChar) {
+                case '~':
+                    comment['section'] = commentValue
+                    break
+                case '#':
+                    comment['title'] = commentValue
+                    break
+                case '-':
+                    comment['description'] = commentValue
+                    break
+                case '`':
+                    comment['code'] = commentValue
+                    break
+                default:
+                    null
+            }
+        })
+
+    return comment
 }
 
 Parser.prototype.grabComment = ( fileData ) => {
@@ -21,27 +52,16 @@ Parser.prototype.grabComment = ( fileData ) => {
                 .tokenize( fileData, options )
 
                 // filter block comments from file
-                .filter( data => (
-                    (data.type === 'BlockComment')
-                        ? data
-                        : null
-                )))
+                .filter( data => ((data.type === 'BlockComment') ? data : null )))
             : reject(new Error('Error while grabbing the comments from file.'))
     })
 }
 
-Parser.prototype.normalizeComment = ( data ) => {
-    let blockComments = []
+Parser.prototype.normalizeComment = ( rawData ) => {
 
-    return new Promise(( resolve, reject ) => { 
-        (data.length)
-            ? resolve(data
-
-                // convert block comment strings into a list of each comment line
-                .map( data =>  ({ 
-                    'blockComment': normalizeCommentLines(data.value)
-                }))
-            )
+    return new Promise(( resolve, reject ) => {
+        (rawData.length)
+            ? resolve(rawData.map( data => (normalizeBlockComments(data.value)) ))
             : reject(new Error('Error while normalizing the comments.'))
     })
 }
