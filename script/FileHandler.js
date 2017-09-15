749e7e02a1fb81891df67c1549943229ab8ce976
springer.ronny@gmail.com <springer.ronny@gmail.com>
2017-09-15 14:55:37 +0200

    add renamed file

---
+++ script/FileHandler.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+const path = require('path')
+
+function FileHandler () {}
+
+FileHandler.prototype.readFile = ( file = '' ) => {
+    const filePath = path.join(__dirname, file)
+    const fileEncoding = {encoding: 'UTF-8'}
+
+    return new Promise(( resolve, reject ) => {
+        fs.readFile(filePath, fileEncoding, ( error, data ) => {
+        	return data
+        		? resolve(data)
+        		: (error) 
+        			? reject(new Error (error))
+        			: reject(new Error ('no data'))
+        })
+    })
+};
+
+exports = module.exports = new FileHandler();
